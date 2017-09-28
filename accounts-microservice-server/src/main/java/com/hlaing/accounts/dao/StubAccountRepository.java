dbe898b1fd45dc352aca727ed8ad0eb7677bf12f
Wintun <wintun@neuro10.com>
2017-09-28 17:19:01 +0800

    fixed reposiroty error

--- accounts-microservice-server/src/main/java/com/hlaing/accounts/dao/StubAccountRepository.java
+++ accounts-microservice-server/src/main/java/com/hlaing/accounts/dao/StubAccountRepository.java
@@ -7,9 +7,11 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import com.hlaing.accounts.model.Account;
 
+@Repository
 public class StubAccountRepository implements AccountRepository {
 	private static final Logger logger = LoggerFactory.getLogger(StubAccountRepository.class);
 	
